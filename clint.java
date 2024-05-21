86a05e68900544156e7b40211ccd9d617745be01
aktikerem <64261277+aktikerem@users.noreply.github.com>
2024-05-21 04:06:35 +0300

    Update clint.java

--- clint.java
+++ clint.java
@@ -11,8 +11,8 @@ public class clint{
 	final int msgID = (int) (Math.random()*100000) + 10000;
         EMAIL email = new EMAIL(smtpServer,port);
 //	email.startReader();
-	email.handleEhlo();
-
+//	email.handleEhlo();
+	email.PacketHandle("EHLO smtp.aktikerem.org\r\n");
 	email.PacketHandle("MAIL FROM:<admin@aktikerem.org>\r\n");
 	email.PacketHandle("RCPT TO:<kerem.akti@gmail.com>\r\n");
 	email.PacketHandle("DATA\r\n");
@@ -23,7 +23,7 @@ public class clint{
 	email.PacketHandle("Date: Tue, 21 May 2024 16:20:00 -0700\r\n ");
 	email.PacketHandle("\r\nHere is our body or not\r\n");
 	email.PacketHandle(".\r\n");
-
+	email.PacketHandle("QUIT");
 
 	            
 
@@ -51,11 +51,14 @@ this.soc = new Socket(this.smtpServer,this.port);
 this.writer = new PrintWriter(new OutputStreamWriter(soc.getOutputStream(), "UTF-8"), true);
 this.reader = new BufferedReader(new InputStreamReader(soc.getInputStream(), "UTF-8"));
 }
-public void handleEhlo() throws InterruptedException{ //handels first ehlo message.
+public void handleEhlo() throws InterruptedException, IOException{ //handels first ehlo message.
 
 Thread.sleep(50);
 writer.print("ehlo smtp.aktikerem.org\r\n");
 writer.flush(); //doesnt listen for the response since it cant fail.
+System.out.println("eglo smtp.aktikerem.org");
+
+handleIS();
 Thread.sleep(100);
 
 
@@ -65,9 +68,10 @@ Thread.sleep(100);
 public void PacketHandle(String msg) throws InterruptedException, IOException{
   writer.print(msg);
   writer.flush();
-  //System.outrintln(msg);
+  System.out.println(msg);
+  Thread.sleep(400);
+
   handleIS();
-  Thread.sleep(100);
 
 
 
@@ -76,14 +80,26 @@ public void PacketHandle(String msg) throws InterruptedException, IOException{
 
 public void handleIS() throws IOException{
 
-System.out.println("debug");
+//System.out.println("debug");
 char[] buf = new char[512];
 InputStreamReader ISR = new InputStreamReader(soc.getInputStream(), "UTF-8");
-bufCountBytes += ISR.read(buf, bufCountBytes,((soc.getInputStream().available())));
+//bufCountBytes += ISR.read(buf, bufCountBytes,((soc.getInputStream().available())));
+bufCountBytes += ISR.read(buf, 0,((soc.getInputStream().available())));
 	
-for(int i = 0;i<buf.length;i++){
-System.out.print(buf[i]);
+//for(int i = 0;i<buf.length;i++){
+String cod = "";
+for(int i = 0; i<buf.length;i++){
+//System.out.print(buf[i]);
+cod += buf[i];
 }
+
+if((cod.substring(0,1).equals("5")) || (cod.substring(0,1).equals("4")) || (cod.substring(0,1).equals("1"))){
+System.out.println("equals '"+cod+"'");
+throw new IOException("Server Err.");
+}
+
+System.out.println(cod);
+cod = "";
 }
 
 public void startReader() {
